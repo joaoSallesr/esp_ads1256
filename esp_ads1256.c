1d59526b226778d0fdfce187c4ababae2b628474
João Salles <joao.sallesr@gmail.com>
2026-04-08 17:36:24 -0300

    feat: add cs management and SPI writing/reading registers

--- esp_ads1256.c
+++ esp_ads1256.c
@@ -1,136 +1,202 @@
 #include "esp_ads1256.h"
 
-#define ESP_ARG_CHECK(VAL)              \
-    do                                  \
-    {                                   \
-        if (!(VAL))                     \
-            return ESP_ERR_INVALID_ARG; \
+#define ESP_ARG_CHECK(VAL)                                                                                             \
+    do {                                                                                                               \
+        if (!(VAL))                                                                                                    \
+            return ESP_ERR_INVALID_ARG;                                                                                \
     } while (0)
 
 static const char *TAG = "ads1256";
 
-// Temporary stubs - implement later
-esp_err_t ads1256_send_cmd(ads1256_handle_t handle, uint8_t cmd)
-{
-    // TODO: implement
+/* ADS1256 timing constants */
+// VERIFICAR ESSAS CONSTANTES NO DATASHEET -----------------------------------------------------------
+#define T6_US      7 // CS↓ to first SCLK (min 50 ns, use  1µs to be safe)
+#define T11_US     4 // Last SCLK to CS↑ (min t11 = 4 × tCLKIN ≈ 1.6 µs @ 7.68 MHz)
+#define TSETTLE_US 4 // After SYNC+WAKEUP, wait 4 × tCLKIN before reading
+
+/* SPI clock: ADS1256 supports up to fCLKIN/4. At default 7.68 MHz CLKIN → 1.92 MHz. */
+// DEFINIR SPI CLOCK HZ ------------------------------------------------------------------------------
+#define SPI_CLOCK_HZ 1920000
+
+/**
+ * @brief CS management (low)
+ */
+static inline void cs_low(ads1256_handle_t handle) {
+    gpio_set_level(handle->dev_config.cs, 0);
+    ets_delay_us(T6_US);
+}
+
+/**
+ * @brief CS management (high)
+ */
+static inline void cs_high(ads1256_handle_t handle) {
+    ets_delay_us(T11_US);
+    gpio_set_level(handle->dev_config.cs, 1);
+}
+
+/**
+ * @brief Raw SPI write
+ */
+static esp_err_t spi_write_bytes(ads1256_handle_t handle, const uint8_t *data, size_t len) {
+    spi_transaction_t t = {
+        .length    = len * 8,
+        .tx_buffer = data,
+    };
+
+    return spi_device_polling_transmit(handle->spi_handle, &t);
+}
+
+/**
+ * @brief Raw SPI read
+ */
+static esp_err_t spi_read_bytes(ads1256_handle_t handle, uint8_t *data, size_t len) {
+    spi_transaction_t t = {
+        .length    = len * 8,
+        .rxlength  = len * 8,
+        .rx_buffer = data,
+    };
+
+    return spi_device_polling_transmit(handle->spi_handle, &t);
+}
+
+/* Main Implementation */
+
+esp_err_t ads1256_wait_drdy(ads1256_handle_t handle) {
+    int64_t deadline_us = esp_timer_get_time() + (int64_t)handle->dev_config.drdy_timeout_ms * 1000;
+    while (gpio_get_level(handle->dev_config.drdy) != 0) {
+        if (esp_timer_get_time() > deadline_us) {
+            ESP_LOGE(TAG, "DRDY timeout");
+            return ESP_ERR_TIMEOUT;
+        }
+        taskYIELD();
+    }
     return ESP_OK;
 }
 
-esp_err_t ads1256_wait_drdy(ads1256_handle_t handle)
-{
-    // TODO: implement
-    return ESP_OK;
+esp_err_t ads1256_read_reg(ads1256_handle_t handle, uint8_t reg, uint8_t *out_val) {
+    esp_err_t ret   = ESP_OK;
+    uint8_t   tx[2] = {ADS1256_CMD_RREG | (reg & 0x0F), 0x00}; // cmd, count-1
+
+    cs_low(handle);
+    ESP_GOTO_ON_ERROR(spi_write_bytes(handle, tx, 2), done, TAG, "Failed to write RREG command");
+    ets_delay_us(T6_US); // t6: min 50 × tCLKIN ≈ 6.5 µs
+    ESP_GOTO_ON_ERROR(spi_read_bytes(handle, out_val, 1), done, TAG, "Failed to read register");
+
+done:
+    cs_high(handle);
+    return ret;
 }
 
-esp_err_t ads1256_write_reg(ads1256_handle_t handle, uint8_t reg, uint8_t val)
-{
-    // TODO: implement
-    return ESP_OK;
+esp_err_t ads1256_write_reg(ads1256_handle_t handle, uint8_t reg, uint8_t val) {
+    uint8_t tx[3] = {
+        ADS1256_CMD_WREG | (reg & 0x0F), // command
+        0x00,                            // count - 1 (writing 1 register)
+        val,
+    };
+
+    cs_low(handle);
+    esp_err_t ret = spi_write_bytes(handle, tx, 3);
+    cs_high(handle);
+    return ret;
 }
 
-esp_err_t ads1256_set_channel(ads1256_handle_t handle, uint8_t pos, uint8_t neg)
-{
-    // TODO: implement
-    return ESP_OK;
+esp_err_t ads1256_send_cmd(ads1256_handle_t handle, uint8_t cmd) {
+    cs_low(handle);
+    esp_err_t ret = spi_write_bytes(handle, &cmd, 1);
+    cs_high(handle);
+    return ret;
 }
 
-esp_err_t ads1256_set_gain(ads1256_handle_t handle)
-{
-    // TODO: implement
-    return ESP_OK;
+esp_err_t ads1256_set_channel(ads1256_handle_t handle, uint8_t pos, uint8_t neg) {
+    uint8_t mux = ((pos & 0x0F) << 4) | (neg & 0x0F);
+    return ads1256_write_reg(handle, ADS1256_REG_MUX, mux);
 }
 
-esp_err_t ads1256_set_drate(ads1256_handle_t handle)
-{
-    // TODO: implement
-    return ESP_OK;
+esp_err_t ads1256_set_gain(ads1256_handle_t handle) {
+    return ads1256_write_reg(handle, ADS1256_REG_ADCON, handle->dev_config.gain);
 }
 
-esp_err_t ads1256_init(const ads1256_config_t *ads1256_config, ads1256_handle_t *ads1256_handle)
-{
+esp_err_t ads1256_set_drate(ads1256_handle_t handle) {
+    return ads1256_write_reg(handle, ADS1256_REG_DRATE, handle->dev_config.drate);
+}
+
+esp_err_t ads1256_init(const ads1256_config_t *ads1256_config, ads1256_handle_t *ads1256_handle) {
     ESP_ARG_CHECK(ads1256_config);
     esp_err_t ret = ESP_FAIL;
 
     /* validate memory allocation */
-    ads1256_handle_t dev_handle;
-    dev_handle = (ads1256_handle_t)calloc(1, sizeof(*dev_handle));
-    ESP_RETURN_ON_FALSE(dev_handle, ESP_ERR_NO_MEM,
-                        TAG, "No memory for ADS1256  device");
+    ads1256_handle_t out_handle;
+    out_handle = (ads1256_handle_t)calloc(1, sizeof(*out_handle));
+    ESP_RETURN_ON_FALSE(out_handle, ESP_ERR_NO_MEM, TAG, "No memory for ADS1256  device");
 
-    /* copy device config to dev_handle */
-    dev_handle->dev_config = *ads1256_config;
+    /* copy device config to out_handle */
+    out_handle->dev_config = *ads1256_config;
 
     /* set SPI device configuration */
 
     // VERIFICAR CONFIGURAÇÃO SPI DO DISPOSITIVO --------------------------------------------------------
 
     const spi_device_interface_config_t ads_dev_config = {
-        .command_bits = 0,
-        .address_bits = 0,
-        .dummy_bits = 0,
-        .mode = 1, // SPI mode 1 for ADS1256
-        .duty_cycle_pos = 128,
-        .cs_ena_pretrans = 0,
+        .command_bits     = 0,
+        .address_bits     = 0,
+        .dummy_bits       = 0,
+        .mode             = 1, // SPI mode 1 for ADS1256
+        .duty_cycle_pos   = 128,
+        .cs_ena_pretrans  = 0,
         .cs_ena_posttrans = 0,
-        .clock_speed_hz = 1920000, // 1.92 MHz
-        .input_delay_ns = 0,
-        .spics_io_num = ads1256_config->cs,
-        .queue_size = 1,
+        .clock_speed_hz   = 1920000, // 1.92 MHz
+        .input_delay_ns   = 0,
+        .spics_io_num     = -1, // disable automatic CS management
+        .queue_size       = 1,
     };
 
     // VERIFICAR CONFIGURAÇÃO SPI DO DISPOSITIVO --------------------------------------------------------
 
     /* add ADS1256 to SPI bus */
-    ESP_GOTO_ON_ERROR(spi_bus_add_device(ads1256_config->spi_host, &ads_dev_config, &dev_handle->spi_handle),
+    ESP_GOTO_ON_ERROR(spi_bus_add_device(ads1256_config->spi_host, &ads_dev_config, &out_handle->spi_handle),
                       err_handle, TAG, "Failed to add ADS1256 device to SPI bus");
 
     /* ADS1256 software reset */
-    ESP_GOTO_ON_ERROR(ads1256_send_cmd(dev_handle, ADS1256_CMD_RESET),
-                      err_handle, TAG, "Failed to soft-reset ADS1256");
+    ESP_GOTO_ON_ERROR(ads1256_send_cmd(out_handle, ADS1256_CMD_RESET), err_handle, TAG, "Failed to soft-reset ADS1256");
 
     /* short delay after software reset */
     vTaskDelay(pdMS_TO_TICKS(1));
 
     /* wait for ADS1256 to be ready */
-    ESP_GOTO_ON_ERROR(ads1256_wait_drdy(dev_handle),
-                      err_handle, TAG, "Failed to get DRDY");
+    ESP_GOTO_ON_ERROR(ads1256_wait_drdy(out_handle), err_handle, TAG, "Failed to get DRDY");
 
     // VERIFICAR STATUS e CHANNELS --------------------------------------------------------
 
     /* set STATUS register after software reset */
     uint8_t bufen_status = ads1256_config->bufen ? 0x02 : 0x00;
-    ESP_GOTO_ON_ERROR(ads1256_write_reg(dev_handle, ADS1256_REG_STATUS, bufen_status),
-                      err_handle, TAG, "Failed to set STATUS register");
+    ESP_GOTO_ON_ERROR(ads1256_write_reg(out_handle, ADS1256_REG_STATUS, bufen_status), err_handle, TAG,
+                      "Failed to set STATUS register");
 
     /* set channels after software reset */
-    ESP_GOTO_ON_ERROR(ads1256_set_channel(dev_handle, ADS1256_MUX_AIN0, ADS1256_MUX_AINCOM),
-                      err_handle, TAG, "Failed to set default channel");
+    ESP_GOTO_ON_ERROR(ads1256_set_channel(out_handle, ADS1256_MUX_AIN0, ADS1256_MUX_AINCOM), err_handle, TAG,
+                      "Failed to set default channel");
 
     // VERIFICAR STATUS e CHANNELS --------------------------------------------------------
 
     /* set GAIN */
-    ESP_GOTO_ON_ERROR(ads1256_set_gain(dev_handle),
-                      err_handle, TAG, "Failed to set GAIN");
+    ESP_GOTO_ON_ERROR(ads1256_set_gain(out_handle), err_handle, TAG, "Failed to set GAIN");
 
     /* set DRATE */
-    ESP_GOTO_ON_ERROR(ads1256_set_drate(dev_handle),
-                      err_handle, TAG, "Failed to set DRATE");
+    ESP_GOTO_ON_ERROR(ads1256_set_drate(out_handle), err_handle, TAG, "Failed to set DRATE");
 
     /* run SELF-CALIBRATION */
-    ESP_GOTO_ON_ERROR(ads1256_send_cmd(dev_handle, ADS1256_CMD_SELFCAL),
-                      err_handle, TAG, "Failed self calibration");
+    ESP_GOTO_ON_ERROR(ads1256_send_cmd(out_handle, ADS1256_CMD_SELFCAL), err_handle, TAG, "Failed self calibration");
 
     /* wait for ADS1256 to be ready */
-    ESP_GOTO_ON_ERROR(ads1256_wait_drdy(dev_handle),
-                      err_handle, TAG, "Failed to get DRDY");
+    ESP_GOTO_ON_ERROR(ads1256_wait_drdy(out_handle), err_handle, TAG, "Failed to get DRDY");
 
     ESP_LOGI(TAG, "ADS1256 initialized");
-    *ads1256_handle = dev_handle;
+    *ads1256_handle = out_handle;
     return ESP_OK;
 
 err_handle:
-    if (dev_handle && dev_handle->spi_handle)
-        spi_bus_remove_device(dev_handle->spi_handle);
-    free(dev_handle);
+    if (out_handle && out_handle->spi_handle)
+        spi_bus_remove_device(out_handle->spi_handle);
+    free(out_handle);
     return ret;
 }
--- include/esp_ads1256.h
+++ include/esp_ads1256.h
@@ -5,118 +5,130 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <esp_check.h>
+#include <esp_err.h>
 #include <esp_log.h>
 #include <esp_system.h>
 #include <esp_timer.h>
-#include <esp_err.h>
-#include <esp_check.h>
-#include <esp_log.h>
 
-#include <driver/spi_master.h>
 #include <driver/gpio.h>
+#include <driver/spi_master.h>
 
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <freertos/timers.h>
 
-// ─── Register Addresses ───────────────────────────────────────────────────────
+/*
+ *  ADS1256 Registers
+ */
 #define ADS1256_REG_STATUS 0x00
-#define ADS1256_REG_MUX 0x01
-#define ADS1256_REG_ADCON 0x02
-#define ADS1256_REG_DRATE 0x03
-#define ADS1256_REG_IO 0x04
-#define ADS1256_REG_OFC0 0x05
-#define ADS1256_REG_OFC1 0x06
-#define ADS1256_REG_OFC2 0x07
-#define ADS1256_REG_FSC0 0x08
-#define ADS1256_REG_FSC1 0x09
-#define ADS1256_REG_FSC2 0x0A
+#define ADS1256_REG_MUX    0x01
+#define ADS1256_REG_ADCON  0x02
+#define ADS1256_REG_DRATE  0x03
+#define ADS1256_REG_IO     0x04
+#define ADS1256_REG_OFC0   0x05
+#define ADS1256_REG_OFC1   0x06
+#define ADS1256_REG_OFC2   0x07
+#define ADS1256_REG_FSC0   0x08
+#define ADS1256_REG_FSC1   0x09
+#define ADS1256_REG_FSC2   0x0A
 
-// ─── Commands ─────────────────────────────────────────────────────────────────
-#define ADS1256_CMD_WAKEUP 0x00
-#define ADS1256_CMD_RDATA 0x01
-#define ADS1256_CMD_RDATAC 0x03
-#define ADS1256_CMD_SDATAC 0x0F
-#define ADS1256_CMD_RREG 0x10 // | reg_addr
-#define ADS1256_CMD_WREG 0x50 // | reg_addr
-#define ADS1256_CMD_SELFCAL 0xF0
+/*
+ *  ADS1256 Commands
+ */
+#define ADS1256_CMD_WAKEUP   0x00
+#define ADS1256_CMD_RDATA    0x01
+#define ADS1256_CMD_RDATAC   0x03
+#define ADS1256_CMD_SDATAC   0x0F
+#define ADS1256_CMD_RREG     0x10 // | reg_addr
+#define ADS1256_CMD_WREG     0x50 // | reg_addr
+#define ADS1256_CMD_SELFCAL  0xF0
 #define ADS1256_CMD_SELFOCAL 0xF1
 #define ADS1256_CMD_SELFGCAL 0xF2
-#define ADS1256_CMD_SYSOCAL 0xF3
-#define ADS1256_CMD_SYSGCAL 0xF4
-#define ADS1256_CMD_SYNC 0xFC
-#define ADS1256_CMD_STANDBY 0xFD
-#define ADS1256_CMD_RESET 0xFE
+#define ADS1256_CMD_SYSOCAL  0xF3
+#define ADS1256_CMD_SYSGCAL  0xF4
+#define ADS1256_CMD_SYNC     0xFC
+#define ADS1256_CMD_STANDBY  0xFD
+#define ADS1256_CMD_RESET    0xFE
 
-// ─── MUX channel definitions ──────────────────────────────────────────────────
-// Usage: mux = (ADS1256_MUX_AIN0 << 4) | ADS1256_MUX_AINCOM  → single-ended AIN0
-#define ADS1256_MUX_AIN0 0x0
-#define ADS1256_MUX_AIN1 0x1
-#define ADS1256_MUX_AIN2 0x2
-#define ADS1256_MUX_AIN3 0x3
-#define ADS1256_MUX_AIN4 0x4
-#define ADS1256_MUX_AIN5 0x5
-#define ADS1256_MUX_AIN6 0x6
-#define ADS1256_MUX_AIN7 0x7
+/*
+ *  ADS1256 Mux Channels
+ */
+#define ADS1256_MUX_AIN0   0x0
+#define ADS1256_MUX_AIN1   0x1
+#define ADS1256_MUX_AIN2   0x2
+#define ADS1256_MUX_AIN3   0x3
+#define ADS1256_MUX_AIN4   0x4
+#define ADS1256_MUX_AIN5   0x5
+#define ADS1256_MUX_AIN6   0x6
+#define ADS1256_MUX_AIN7   0x7
 #define ADS1256_MUX_AINCOM 0x8
 
-// ─── PGA gain options (ADCON register bits 2:0) ───────────────────────────────
-typedef enum
-{
-    ADS1256_GAIN_1 = 0,
-    ADS1256_GAIN_2 = 1,
-    ADS1256_GAIN_4 = 2,
-    ADS1256_GAIN_8 = 3,
+/*
+ * ADS1256 Timing Delays
+ */
+#define ADS1256_T6_DELAY_US    7 // after RDATA
+#define ADS1256_T10_DELAY_US   2 // CS hold
+#define ADS1256_RESET_DELAY_US 1 // reset pulse
+
+/*
+ *  ADS1256 Gain Options
+ */
+typedef enum {
+    ADS1256_GAIN_1  = 0,
+    ADS1256_GAIN_2  = 1,
+    ADS1256_GAIN_4  = 2,
+    ADS1256_GAIN_8  = 3,
     ADS1256_GAIN_16 = 4,
     ADS1256_GAIN_32 = 5,
     ADS1256_GAIN_64 = 6,
 } ads1256_gain_t;
 
-// ─── Data rate options (DRATE register) ──────────────────────────────────────
-typedef enum
-{
+/*
+ *  ADS1256 Data Rate Options
+ */
+typedef enum {
     ADS1256_DRATE_30000SPS = 0xF0,
     ADS1256_DRATE_15000SPS = 0xE0,
-    ADS1256_DRATE_7500SPS = 0xD0,
-    ADS1256_DRATE_3750SPS = 0xC0,
-    ADS1256_DRATE_2000SPS = 0xB0,
-    ADS1256_DRATE_1000SPS = 0xA1,
-    ADS1256_DRATE_500SPS = 0x92,
-    ADS1256_DRATE_100SPS = 0x82,
-    ADS1256_DRATE_60SPS = 0x72,
-    ADS1256_DRATE_50SPS = 0x63,
-    ADS1256_DRATE_30SPS = 0x53,
-    ADS1256_DRATE_25SPS = 0x43,
-    ADS1256_DRATE_15SPS = 0x33,
-    ADS1256_DRATE_10SPS = 0x23,
-    ADS1256_DRATE_5SPS = 0x13,
-    ADS1256_DRATE_2_5SPS = 0x03,
+    ADS1256_DRATE_7500SPS  = 0xD0,
+    ADS1256_DRATE_3750SPS  = 0xC0,
+    ADS1256_DRATE_2000SPS  = 0xB0,
+    ADS1256_DRATE_1000SPS  = 0xA1,
+    ADS1256_DRATE_500SPS   = 0x92,
+    ADS1256_DRATE_100SPS   = 0x82,
+    ADS1256_DRATE_60SPS    = 0x72,
+    ADS1256_DRATE_50SPS    = 0x63,
+    ADS1256_DRATE_30SPS    = 0x53,
+    ADS1256_DRATE_25SPS    = 0x43,
+    ADS1256_DRATE_15SPS    = 0x33,
+    ADS1256_DRATE_10SPS    = 0x23,
+    ADS1256_DRATE_5SPS     = 0x13,
+    ADS1256_DRATE_2_5SPS   = 0x03,
 } ads1256_drate_t;
 
-// ─── Configuration struct ─────────────────────────────────────────────────────
+/*
+ *  ADS1256 Structures
+ */
 
 /**
  * @brief ADS1256 configuration structure.
  */
-typedef struct ads1256_config_s
-{
-    spi_host_device_t spi_host;
-    gpio_num_t cs;
-    gpio_num_t drdy;  // Data Ready (active low)
-    gpio_num_t reset; // Hardware reset (active low), or GPIO_NUM_NC to skip
-    ads1256_gain_t gain;
-    ads1256_drate_t drate;
-    int16_t drdy_timeout_ms;
-    int16_t ads_transfer_size;
-    bool bufen;
+typedef struct ads1256_config_s {
+    spi_host_device_t spi_host; /*!< SPI bus selected */
+    gpio_num_t        cs;       /*!< ads1256 CS pin */
+    gpio_num_t        drdy;     /*!< Data Ready (active low) */
+    ads1256_gain_t    gain;     /*!< ADS1256 Gain Options */
+    ads1256_drate_t   drate;    /*!< ADS1256 Data Rate Options */
+    int32_t           drdy_timeout_ms;
+    int32_t           ads_transfer_size;
+    bool              bufen;
 } ads1256_config_t;
 
 /**
  * @brief ADS1256 context structure.
  */
-struct ads1256_context_t
-{
-    ads1256_config_t dev_config;
+struct ads1256_context_t {
+    ads1256_config_t    dev_config;
     spi_device_handle_t spi_handle;
 };
 
@@ -130,17 +142,40 @@ typedef struct ads1256_context_t ads1256_context_t;
  */
 typedef struct ads1256_context_t *ads1256_handle_t;
 
-/**
- * @brief ADS1256 handle structure definition.
+/*
+ *  ADS1256 Functions Declarations
  */
-esp_err_t ads1256_init(const ads1256_config_t *ads1256_config, ads1256_handle_t *ads1256_handle);
 
-// FAZER ----------------------------------------------------------------------------------
+/**
+ * @brief Initalize ADS1256 device and add it to SPI
+ *
+ * @param[in] config ADS1256 device config.
+ * @param[out] handle ADS1256 device handle.
+ * @return esp_err_t ESP_OK on success.
+ */
+esp_err_t ads1256_init(const ads1256_config_t *config, ads1256_handle_t *handle);
+
+/**
+ * @brief Block communication until DRDY goes low
+ * @param[in] handle ADS1256 device handle.
+ * @return esp_err_t ESP_OK on success.
+ */
+esp_err_t ads1256_wait_drdy(ads1256_handle_t handle);
+// FAZER -------------------------------------------------------
+
+/**
+ * @brief Remove ADS1256 device from SPI and free resources
+ *
+ * @param[in] handle ADS1256 device handle.
+ * @return esp_err_t ESP_OK on success.
+ */
+esp_err_t ads1256_delete(ads1256_handle_t handle);
+
+esp_err_t ads1256_read_single(ads1256_handle_t handle, int32_t *out_raw);
+
 esp_err_t ads1256_send_cmd(ads1256_handle_t handle, uint8_t cmd);
 
 esp_err_t ads1256_write_reg(ads1256_handle_t handle, uint8_t reg, uint8_t val);
-
-esp_err_t ads1256_wait_drdy(ads1256_handle_t handle);
 
 esp_err_t ads1256_set_gain(ads1256_handle_t handle);
 
